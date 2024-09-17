ecd34f0bec153360149931003ce879f08649795b
Jonathan Williams <tamlin@mac.com>
2024-09-17 16:11:29 -0400

    setup a flexible proxy for local development
    to avoid CORS issues when accessing lambda gateways from localhost

--- src/lib/getConsentData.js
+++ src/lib/getConsentData.js
@@ -1,7 +1,11 @@
 async function getConsentData(accessToken, email) {
-    // This is the URL of the API Gateway endpoint that will be called.
-    //const response = await fetch(import.meta.env.VITE_CONSENTING_API_URI, {
-    const response = await fetch('/api/dev/CONSENTING_PERSON', {
+    // Set the API URL based on the environment, local dev needs a proxy to avoid CORS issues.
+    const apiUrl = import.meta.env.MODE === 'development'
+        ? '/consentingApi/dev/CONSENTING_PERSON'
+        : `${import.meta.env.VITE_CONSENTING_API_HOST}/dev/CONSENTING_PERSON`;
+
+    // Fetch the consenting person data from the API with the token from sign in.
+    const response = await fetch(apiUrl, {
         method: 'GET',
         mode: 'cors',
         credentials: 'include',
@@ -12,6 +16,7 @@ async function getConsentData(accessToken, email) {
         }
     });
 
+    // Extract and return the payload from the response.
     const data = await response.json();
     return data.payload;
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Can load proxy targets from environment variables:
+// https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js/66389044#66389044
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const { VITE_CONSENTING_API_HOST } = env;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/consentingApi': {
+          target: VITE_CONSENTING_API_HOST,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/consentingApi/, '')
+        }
+      }
+    }
+  }
+});
