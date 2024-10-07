7689acd6dfa21c1adcdbe7fb5ae671c77c50b11a
Jonathan Williams <tamlin@mac.com>
2024-10-07 12:51:59 -0400

    initial form handling

--- src/pages/authorized/signUpAuthInd/signUpAuthIndForm.jsx
+++ src/pages/authorized/signUpAuthInd/signUpAuthIndForm.jsx
@@ -1,16 +1,47 @@
-import { Card, CardBody, CardHeader, Code, FormControl, Heading, Text } from "@chakra-ui/react";
+import { useState } from 'react';
+import { Button, Card, CardBody, CardHeader, FormControl, FormLabel, Heading, Input, Text } from "@chakra-ui/react";
 
 export default function SignUpAuthIndForm({entityInfo}) {
-
     const  { entity, invitation, users }  = entityInfo;
+
+    // Initialize state for each form input
+    const [formData, setFormData] = useState({
+        fullname: '',
+        title: '',
+        email: ''
+    });
 
     // Find the user with a role property of 'RE_ADMIN', which is the administator of the entity.
     const entityAdmin = users.find(user => user.role === 'RE_ADMIN');
 
+    // Handle form input changes
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    // Handle form submission
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log('Registering: ', formData);
+
+        // setApiState('loading');
+
+        // const registerResult = await registerEntityAPI(code, formData);
+        // console.log(registerResult);
+
+        // if (registerResult.payload.ok) {
+
+        //     console.log('Registration successful');
+        //     setStepIndex(3);
+        //     setApiState('success');
+        // }
+    }
+
     return (
         <>
             <Heading as="h3" mb="1em" size="md">Register Account</Heading>
-            <Card variant="filled">
+            <Card mb="1em" variant="filled">
                 <CardHeader>
                     <Heading as="h4" size="sm">{entity.entity_name}</Heading>
                 </CardHeader>
@@ -22,7 +53,28 @@ export default function SignUpAuthIndForm({entityInfo}) {
                     <Text>{entityAdmin.phone_number}</Text>
                 </CardBody>
             </Card>
-            <FormControl></FormControl>
+            <FormControl as="form" onSubmit={handleSubmit}>
+                <FormLabel>Your Full Name</FormLabel>
+                <Input
+                    name="fullname"
+                    value={formData.fullname}
+                    onChange={handleChange}
+                />
+                <FormLabel>Your Title</FormLabel>
+                <Input
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                />
+                <FormLabel>Your Email</FormLabel>
+                <Input
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                <Button my="1em" type="submit">Sign Up</Button>
+            </FormControl>
         </>
     );
 }
