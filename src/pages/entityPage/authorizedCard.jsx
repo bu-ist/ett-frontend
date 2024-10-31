568b36daf7492bd49183b3788e301ec071b10e51
Jonathan Williams <tamlin@mac.com>
2024-10-31 13:56:15 -0400

    improve feedback for authorized individuals

--- src/pages/entityPage/authorizedCard.jsx
+++ src/pages/entityPage/authorizedCard.jsx
@@ -1,10 +1,12 @@
-import { Card, CardHeader, Heading, CardBody, CardFooter, Text } from "@chakra-ui/react";
+import { Card, CardHeader, Heading, CardBody, CardFooter, Text, Icon } from "@chakra-ui/react";
+import { HiMinusCircle } from "react-icons/hi";
 
 import InviteUsersModal from "./inviteUsersModal";
 
 export default function AuthorizedCard({ entity }) {
 
     const authInds = entity.users.filter(user => user.role === 'RE_AUTH_IND');
+    const authIndsNames = authInds.map(member => member.fullname);
 
     return (
         <Card>
@@ -12,13 +14,23 @@ export default function AuthorizedCard({ entity }) {
                 <Heading as="h4" size="md">Authorized Individuals</Heading>
             </CardHeader>
             <CardBody>
-                {entity.users.length == 0 && <Text>None</Text>}
-                {authInds.map((member, index) => (
-                    <Text key={index}>{member.fullname}</Text>
+                {entity.users.length == 0 && 
+                    <Stack direction="row"><Icon as={HiMinusCircle} color="gray.400" boxSize="6" /><Text>No Authorized Individuals currently registered</Text></Stack>
+                }
+                {entity.users.length == 1 && 
+                    <>
+                        <Text>{authIndsNames[0]}</Text>
+                        <Stack mt="2" direction="row"><Badge>pending</Badge><Text as="i" fontSize="sm">Second invitation not yet accepted</Text></Stack>
+                    </>
+                }
+                {entity.users.length == 2 && authIndsNames.map((fullname, index) => (
+                    <Text key={index}>{fullname}</Text>
                 ))}
             </CardBody>
             <CardFooter>
+            {entity.users.length == 0 && 
                 <InviteUsersModal numUsers={entity.users.length} entity={entity} />
+            }
             </CardFooter>
         </Card>
     );
