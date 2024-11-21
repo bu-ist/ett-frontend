2e7d5715dc19271bd52840d83c228089e9840011
Jonathan Williams <tamlin@mac.com>
2024-11-21 17:41:47 -0500

    add sign in and user context

--- src/pages/consenting/newContactListPage.jsx
+++ src/pages/consenting/newContactListPage.jsx
@@ -1,8 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { nanoid } from "nanoid";
 import { Text, Box, Heading, Spinner, FormControl, FormLabel, Input, Card, CardBody, CardHeader, Button, Divider, CardFooter, RadioGroup, Stack, Radio } from "@chakra-ui/react";
 
+import { UserContext } from '../../lib/userContext';
+
+import { signIn } from '../../lib/signIn';
+import { exchangeAuthorizationCode } from '../../lib/exchangeAuthorizationCode';
 import { getConsentData } from '../../lib/getConsentData';
 import { sendExhibitFormAPI } from '../../lib/consenting/sendExhibitFormAPI';
 
@@ -11,6 +16,7 @@ import EntityAutocomplete from './newContactListPage/entityAutocomplete';
 
 
 export default function NewContactListPage() {
+    const { setUser } = useContext(UserContext);
     const [apiState, setApiState] = useState('');
     const [consentData, setConsentData] = useState({});
 
@@ -29,12 +35,35 @@ export default function NewContactListPage() {
         }
     ]);
 
+    let [searchParams, setSearchParams] = useSearchParams();
+
     useEffect(() => {
         // Should probably add a handler for the case where the user is not signed in.
+
+        let accessToken = Cookies.get('EttAccessJwt');
+        let idToken = Cookies.get('EttIdJwt');
+
+        // If there is no access token and no code in the URL, the user is not signed in.
+        if (!accessToken && ! searchParams.has('code')) {
+            // Set the api response state to reflect that the user is not signed in.
+            setApiState('notSignedIn');
+            return;
+        }
+
         async function fetchData() {
             setApiState('loading');
-            const accessToken = Cookies.get('EttAccessJwt');
-            const idToken = Cookies.get('EttIdJwt');
+
+            // First check if there is a code in the URL, and if so, exchange it for tokens.
+            if (searchParams.has('code')) {
+                const clientId = import.meta.env.VITE_CONSENTING_COGNITO_CLIENTID;
+                await exchangeAuthorizationCode(clientId, 'consenting/add-exhibit-form');
+                // Use setSearchParams to empty the search params once exchangeAuthorizationCode is done with them.
+                setSearchParams({});
+
+                // There should be cookies now, so get them again.
+                accessToken = Cookies.get('EttAccessJwt');
+                idToken = Cookies.get('EttIdJwt');
+            }
 
             // If the user is signed in, get their consent data.
             if (accessToken && idToken) {
@@ -42,6 +71,10 @@ export default function NewContactListPage() {
 
                 const consentResponse = await getConsentData(accessToken, decodedIdToken.email);
                 setConsentData(consentResponse);
+
+                // Set the user context for the site header avatar.
+                setUser({email: consentResponse.consenter.email, fullname: consentResponse.fullName});
+
                 setApiState('success');
             } else {
                 setApiState('error');
@@ -169,6 +202,19 @@ export default function NewContactListPage() {
 
     return (
         <Box>
+            {apiState === 'notSignedIn' &&
+                <Card my={6} align="center">
+                    <CardHeader>
+                        <Heading as="h2" color="gray.500" >Not Signed In</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Text>Sign in to access this page.</Text>
+                    </CardBody>
+                    <CardFooter>
+                        <Button onClick={() => signIn(import.meta.env.VITE_CONSENTING_COGNITO_CLIENTID, 'consenting/add-exhibit-form')}>Sign in as a Consenting Person</Button>
+                    </CardFooter>
+                </Card>
+            }
             {apiState === 'loading' && <Spinner />}
             {apiState === 'error' && <Text>There was an error loading the new contact list page.</Text>}
             {apiState === 'success' && 
