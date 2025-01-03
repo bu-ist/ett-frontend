41b0c42392320c8d48cca41bce2f5325b4bbc722
Jonathan Williams <tamlin@mac.com>
2025-01-03 17:17:13 -0500

    use existing privacy policy component
    from the entity

--- src/pages/authorized/signUpAuthInd/acknowledgePrivacy.jsx
+++ src/pages/authorized/signUpAuthInd/acknowledgePrivacy.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react';
-import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Link, Text, Spinner } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+
+import EntityPrivacyPolicy from "../../entity/acknowledgeEntity/entityPrivacyPolicy";
 
 export default function AcknowledgePrivacy({ acceptPrivacyPolicy }) {
-    const [apiState, setApiState] = useState('idle');
-
-    async function handleAcceptClick() {
-        acceptPrivacyPolicy();
-        setApiState('success');
-    }
 
     return (
-        <Card mb="1em">
-            <CardHeader>
-                <Heading as="h4" size="sm">Privacy Policy</Heading>
-            </CardHeader>
-            <CardBody>
-                <Text>
-                    By clicking the &quot;Accept&quot; button, you agree to the Privacy Policy of the entity.
-                </Text>
-                <Text>
-                    You can view the Privacy Policy <Link href="/privacy-policy">here</Link>.
-                </Text>
-            </CardBody>
-            <CardFooter>
-                <Button
-                    onClick={handleAcceptClick}
-                >
-                    {apiState === 'loading' && <Spinner />}
-                    {apiState !== 'loading' && 'Accept'}
-                </Button>
-            </CardFooter>
-        </Card>
+        <>
+            <EntityPrivacyPolicy accepted={false} /> 
+            <Button my="6" onClick={acceptPrivacyPolicy}>
+                Accept
+            </Button>
+        </>
     );
 }
