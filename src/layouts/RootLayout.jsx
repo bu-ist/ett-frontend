3016746ee5a017be03217c2861d0fb84fc7db143
Jonathan Williams <tamlin@mac.com>
2025-01-10 12:27:01 -0500

    fix spacing

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -38,24 +38,24 @@ export default function RootLayout() {
 
     return (
         <ConfigContext.Provider value={{ appConfig }}>
-        <UserContext.Provider value={{ user, setUser }}>
-            <Container maxWidth="5xl">
-                <Box backgroundColor="gray.100" padding={3.5} mb={4}>
-                    <Flex mt={4} minWidth='max-content' alignItems='center' gap='2'>
-                        <Heading as="h1" size="2xl">
-                            <Link to="/">Ethical Transparency Tool</Link>
-                        </Heading>
-                        <Spacer />
-                        <UserAvatar user={user} />
-                    </Flex>
-                    <NavBreadcrumb />
-                </Box>
-                <div>
-                    <Outlet />
-                </div>
-                <SiteFooter />
-            </Container>
-        </UserContext.Provider>
+            <UserContext.Provider value={{ user, setUser }}>
+                <Container maxWidth="5xl">
+                    <Box backgroundColor="gray.100" padding={3.5} mb={4}>
+                        <Flex mt={4} minWidth='max-content' alignItems='center' gap='2'>
+                            <Heading as="h1" size="2xl">
+                                <Link to="/">Ethical Transparency Tool</Link>
+                            </Heading>
+                            <Spacer />
+                            <UserAvatar user={user} />
+                        </Flex>
+                        <NavBreadcrumb />
+                    </Box>
+                    <div>
+                        <Outlet />
+                    </div>
+                    <SiteFooter />
+                </Container>
+            </UserContext.Provider>
         </ConfigContext.Provider>
     );
 }
