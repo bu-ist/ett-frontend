becc4cd597e7905db009afc14ecffbe75175e2e5
Jonathan Williams <tamlin@mac.com>
2025-01-28 12:13:01 -0500

    make the example privacy policy text longer

--- src/pages/entity/acknowledgeEntity/entityPrivacyPolicy.jsx
+++ src/pages/entity/acknowledgeEntity/entityPrivacyPolicy.jsx
@@ -32,6 +32,106 @@ Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet
 
 Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
                 </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
+                <Divider my="4" />
+                <Text>
+                    Nisi ex qui dolore irure dolor ut id velit veniam consequat. Veniam aliqua sint magna culpa proident dolore qui laborum ut mollit esse ea. Dolor pariatur aliquip non dolor nulla ipsum. Aute esse mollit commodo ad minim aute ut. Ullamco exercitation aliqua deserunt incididunt anim non aliquip.
+
+Sit incididunt exercitation reprehenderit ea eiusmod veniam sint voluptate. Cillum excepteur eiusmod qui sunt qui ut et sunt sunt fugiat deserunt sit nisi. Dolore laboris ex sit labore. Cupidatat culpa irure ea et tempor. Nostrud consectetur esse est est. Ut veniam incididunt ut velit voluptate officia et. Fugiat excepteur laborum sunt incididunt ullamco.
+
+Nisi irure anim tempor qui eiusmod cupidatat anim proident. Quis qui cillum amet eu. Aute est elit ex id eu ea in nisi minim do labore adipisicing culpa. Irure sint elit cillum culpa do sit. Enim culpa consequat cupidatat do nostrud duis est Lorem et et qui.
+
+Consequat irure laboris labore culpa labore amet quis exercitation esse labore officia consectetur quis mollit. Adipisicing laborum est sit nostrud consequat. Voluptate ad adipisicing cupidatat minim laboris sint. Et cillum duis ad voluptate ullamco pariatur laborum proident.
+                </Text>
         </Box>
     );
 }
