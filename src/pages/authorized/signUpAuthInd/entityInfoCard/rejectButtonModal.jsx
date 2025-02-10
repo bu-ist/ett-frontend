cd0d6aba41a7b4b727a4d00c41d5f9b17eac97e3
Jonathan Williams <tamlin@mac.com>
2025-02-10 16:09:50 -0500

    additional UI components, and redirect to home page
    when closing the modal after a successful rejection

--- src/pages/authorized/signUpAuthInd/entityInfoCard/rejectButtonModal.jsx
+++ src/pages/authorized/signUpAuthInd/entityInfoCard/rejectButtonModal.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from 'react';
-import { Button, ButtonGroup, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
+import { Button, ButtonGroup, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import { AiOutlineClose } from 'react-icons/ai';
 
 import { terminateEntityAPI } from '../../../../lib/entity/terminateEntityAPI';
@@ -17,6 +18,11 @@ export default function RejectButtonModal({ inviteInfo }) {
     // API call state
     const [apiState, setApiState] = useState('idle');
 
+    const [terminateResult, setTerminateResult] = useState({});
+
+    // Use navigate hook for redirection
+    const navigate = useNavigate();
+
     async function handleRejectClick(event) {
         event.preventDefault();
         setApiState('loading');
@@ -27,7 +33,8 @@ export default function RejectButtonModal({ inviteInfo }) {
         try {
             // Call the terminateEntityAPI function with the invitation code.
             const response = await terminateEntityAPI( appConfig, code );
-            if (response.ok) {
+            if (response.message === 'Ok') {
+                setTerminateResult(response.payload);
                 setApiState('success');
             } else {
                 setApiState('error');
@@ -35,6 +42,16 @@ export default function RejectButtonModal({ inviteInfo }) {
         } catch (error) {
             console.error(error);
             setApiState('error');
+        }
+    }
+
+    // Handle the modal close event; if there was a successful rejection, redirect to the home page.
+    function handleClose() {
+        if (apiState === 'success') {
+            navigate('/');
+        } else {
+            // Otherwise, just close the modal.
+            onClose();
         }
     }
 
@@ -46,21 +63,28 @@ export default function RejectButtonModal({ inviteInfo }) {
             >
                 Reject
             </Button>
-            <Modal size="xl" isOpen={isOpen} onClose={onClose}>
+            <Modal size="xl" isOpen={isOpen} onClose={handleClose}>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Reject Invitation from {inviteInfo.entity.entity_name}</ModalHeader>
+                    <ModalCloseButton />
                     <ModalBody>
-                        {apiState === 'idle' &&
-                            <Text>
-                                Rejecting the invitation will remove all information associated with the invitation code and the
-                                incomplete Registered Entity. This cannot be undone. Are you sure you want to reject the invitation?
-                            </Text>
-                        }
+                        <Text>
+                            Rejecting the invitation will remove all information associated with the invitation code and the
+                            incomplete Registered Entity, including the accounts of anyone who has already registered. This cannot be undone. Are you sure you want to reject the invitation?
+                        </Text>
                         {apiState === 'success' &&
-                            <Text>
-                                The invitation has been rejected.
-                            </Text>
+                            <>
+                                <Text mt="4" color="red.900">
+                                    <b>The invitation has been rejected.</b>
+                                </Text>
+                                <Text mt="4">
+                                    {terminateResult?.deletedUsers?.length} other user{terminateResult?.deletedUsers?.length > 1 ? 's' : ''} {terminateResult?.deletedUsers?.length > 1 ? 'were' : 'was'} deleted and notified.
+                                </Text>
+                                <Text mt="4">
+                                    Your information has been erased from our system, you will be redirected to the home page.
+                                </Text>
+                            </>
                         }
                         {apiState === 'error' &&
                             <Text>
@@ -73,6 +97,7 @@ export default function RejectButtonModal({ inviteInfo }) {
                             {apiState === 'idle' &&
                                 <Button onClick={onClose}>Cancel</Button>
                             }
+                            {apiState !== 'success' &&
                             <Button
                                 isLoading={apiState === 'loading'}
                                 onClick={handleRejectClick}
@@ -80,8 +105,11 @@ export default function RejectButtonModal({ inviteInfo }) {
                             >
                                 {apiState === 'idle' && 'Reject'}
                                 {apiState === 'error' && 'Error'}
-                                {apiState === 'success' && 'Goodbye'}
                             </Button>
+                            }
+                            {apiState === 'success' &&
+                                <Button colorScheme="red" onClick={handleClose}>Goodbye</Button>
+                            }
                         </ButtonGroup>
                     </ModalFooter>
                 </ModalContent>
