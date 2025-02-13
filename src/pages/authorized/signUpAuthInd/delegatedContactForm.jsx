b73baa3d7bb2bb07e589b343bd4804f7b49a37a0
Jonathan Williams <tamlin@mac.com>
2025-02-13 12:31:54 -0500

    add real text describing the AI's delegated contact

--- src/pages/authorized/signUpAuthInd/delegatedContactForm.jsx
+++ src/pages/authorized/signUpAuthInd/delegatedContactForm.jsx
@@ -6,7 +6,10 @@ export default function DelegatedContactForm({ register, errors }) {
     return (
         <Box>
             <Text mb="4">
-                Amet consequat ea velit sit eu non quis reprehenderit id officia. Qui ut exercitation pariatur commodo deserunt adipisicing aliquip ex veniam sunt dolore reprehenderit. Elit fugiat velit elit culpa eu dolore culpa esse minim labore consequat exercitation in. Mollit irure eiusmod deserunt velit voluptate consectetur dolor sunt cupidatat aliquip deserunt ex.
+                These are the contacts who will respond to Disclosure Requests from another ETT-Registered Entity when your 
+                organization is disclosing its findings of misconduct against a person who is being considered by the other 
+                ETT-Registered Entity.   You may add an existing Authorized Individuals or Administrative Support Professional
+                if these individuals will fulfill the Disclosure Response Contact role too.
             </Text>
             <FormControl mb="4" isInvalid={errors.delegate_fullname}>
                 <FormLabel>Contact Full Name</FormLabel>
