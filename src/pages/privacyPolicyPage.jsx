37dbba42f78f4bc31379ac3a3af9bb062a7e3327
Jonathan Williams <tamlin@mac.com>
2025-02-13 12:52:29 -0500

    update privacy policy policy

--- src/pages/privacyPolicyPage.jsx
+++ src/pages/privacyPolicyPage.jsx
@@ -1,17 +1,12 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Text } from '@chakra-ui/react';
 
-import PrivacyPolicyBox from "../components/sharedTexts/privacyPolicyBox";
+import PrivacyPolicyBox from '../components/sharedTexts/privacyPolicyBox';
+import PrivacyNoticeText from '../components/sharedTexts/privacyNoticeText';
 
 export default function PrivacyPolicyPage() {
     return (
         <>
-            <Text>
-                Ex et consectetur incididunt aute fugiat dolor laboris incididunt consequat est velit dolore officia deserunt.
-
-                Aliqua deserunt esse ad sunt proident voluptate minim voluptate anim deserunt eiusmod consectetur pariatur. Amet non non nisi incididunt officia nostrud laboris. Anim ut occaecat esse aliquip dolor ex cupidatat. Amet esse mollit incididunt consectetur do proident sunt sunt voluptate in magna nostrud aute. Consectetur dolore aliqua deserunt laboris eiusmod ut aute eiusmod. Et ullamco nulla consectetur quis.
-
-                Duis anim excepteur mollit excepteur sit cupidatat Lorem ea proident minim.
-            </Text>
+            <PrivacyNoticeText />
             <PrivacyPolicyBox />
         </>
     );
