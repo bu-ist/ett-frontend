3ae03afe591dbb498df47bf87fa4bd925c7cffcd
Jonathan Williams <tamlin@mac.com>
2025-02-19 10:17:45 -0500

    add stepper and privacy policy to CP register

---
+++ src/pages/consenting/consentingRegisterPage/consentingRegisterStepper.jsx
@@ -0,0 +1,32 @@
+import { Box, Step, StepDescription, StepIcon, StepIndicator, StepNumber, Stepper, StepSeparator, StepStatus, StepTitle } from "@chakra-ui/react";
+
+const steps =[
+    { title: 'Privacy Policy', description: '' },
+    { title: 'Register', description: '' },
+    { title: 'Create Account', description: '' },
+];
+
+export default function ConsentingRegisterStepper({ currentIndex }) {
+    return (
+        <Box my="2em" borderWidth="2px" borderRadius='lg' p='1em' boxShadow='lg'>
+            <Stepper index={currentIndex}>
+                {steps.map((step, index) => (
+                    <Step key={index}>
+                        <StepIndicator>
+                            <StepStatus
+                                complete={<StepIcon />}
+                                incomplete={<StepNumber />}
+                                active={<StepNumber />}
+                            />
+                        </StepIndicator>
+                        <Box flexShrink='0'>
+                            <StepTitle>{step.title}</StepTitle>
+                            <StepDescription>{step.description}</StepDescription>
+                        </Box>
+                        <StepSeparator />
+                    </Step>
+                ))}
+            </Stepper>
+        </Box>
+    );
+}
