7f2226b25cd40c2ba28c593a101ea82815e9233f
Jonathan Williams <tamlin@mac.com>
2025-02-20 15:47:46 -0500

    improve interface for granting consent

--- src/pages/consenting/consentFormPage/grantConsentButton.jsx
+++ src/pages/consenting/consentFormPage/grantConsentButton.jsx
@@ -1,6 +1,6 @@
-import { useState, useContext } from 'react';
-import { Link as ReactRouterLink, useNavigate } from 'react-router-dom';
-import { Button, FormControl, FormLabel, Input, Spinner } from '@chakra-ui/react';
+import { useState, useContext, useEffect, useRef } from 'react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, FormControl, FormLabel, Heading, Input, Spinner } from '@chakra-ui/react';
 import Cookies from 'js-cookie';
 
 import { ConfigContext } from '../../../lib/configContext';
@@ -14,6 +14,8 @@ export default function GrantConsentButton({ consentData }) {
     const [apiState, setApiState] = useState('idle');
 
     const [digitalSignature, setDigitalSignature] = useState('');
+
+    const scrollBoxRef = useRef(null);
 
     async function handleConsentButton() {
         setApiState('loading');
@@ -33,11 +35,17 @@ export default function GrantConsentButton({ consentData }) {
         } 
     }
 
+    useEffect(() => {
+        if (apiState === 'success') {
+            scrollBoxRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [apiState]);
+
     return (
         <>
             <FormControl mt="2em">
                 <FormLabel>Digital Signature</FormLabel>
-                <Input value={digitalSignature} onChange={(e) => setDigitalSignature(e.target.value) } type="text" />
+                <Input value={digitalSignature} isDisabled={apiState !== "idle"} onChange={(e) => setDigitalSignature(e.target.value) } type="text" />
             </FormControl>
             <Button onClick={handleConsentButton} my="2em" isDisabled={apiState !== 'idle'}>
                 {apiState === 'idle' && 'Grant Consent'}
@@ -45,7 +53,20 @@ export default function GrantConsentButton({ consentData }) {
                 {apiState === 'error' && 'Error'}
                 {apiState === 'success' && 'Consent Granted'}
             </Button>
-            <Button ml="6" as={ReactRouterLink} to="/consenting" my="2em"> {"< "} Dashboard</Button>
+            {apiState === 'success' && 
+                <Card>
+                    <CardHeader>
+                        <Heading as="h4" size={"sm"}>Consent Granted</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        You have successfully granted consent. You will receive an email confirmation shortly.
+                    </CardBody>
+                    <CardFooter>
+                        <Button as={ReactRouterLink} to="/consenting" my="2em"> Return to Dashboard</Button>
+                    </CardFooter>
+                </Card>
+            }
+            <Box ref={scrollBoxRef}></Box> {/* Invisible element for scrolling */}
         </>
     );
 }
