a0d0cc53cdcc114155f8b00b0c8d9e309d7faf11
Jonathan Williams <tamlin@mac.com>
2025-03-04 12:06:23 -0500

    additions to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Link as ReactRouterLink, useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import { Box, Button, Card, CardBody, CardFooter, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, CardFooter, Heading, Image, ListItem, SimpleGrid, Stack, Text, UnorderedList } from "@chakra-ui/react";
 
 import { ConfigContext } from "../lib/configContext";
 
@@ -56,10 +56,15 @@ export default function Home() {
     return (
         <>
             <Box my={"2em"}>
+                <Heading size="md">Welcome to the Ethical Transparency Tool (ETT)</Heading>
                 <Text>
-                ETT is designed to support AAU’s harassment prevention principles and the recommendations of NASEM’s June 2018 report on sexual harassment of women in academic science, engineering, and medicine by helping to create a norm of transparency about findings of misconduct against a person, across the higher-education and research ecosystem of societies, institutions of higher education, and other research organizations. This tool covers sexual, gender, and racial misconduct — as well as professional licensure, financial, and research misconduct to maximize its utility.    
+                    ETT is an ethical and efficient communication tool for societies, colleges, and universities to lead by helping to create a norm of 
+                    transparency about findings (not allegations) of misconduct about individuals (sexual/gender and race/ethnicity, as well as financial, 
+                    scientific/research, and licensure), wherever it occurs.  ETT is designed to implement AAU’s harassment prevention principles and the 
+                    recommendations of NASEM’s June 2018 report on sexual harassment of women in academia and to support inclusive learning and research for all talent.   
                 </Text>
             </Box>
+            <Heading mb="4" size="md">Access ETT</Heading>
             <SimpleGrid spacing={4} templateColumns={"repeat(auto-fill, minmax(300px, 1fr))"}>
                 <Card
                     maxWidth={"sm"}
@@ -138,6 +143,40 @@ export default function Home() {
                     </Stack>
                 </Card>
             </SimpleGrid>
+            <Box my="6">
+                <Heading my="4" size="md">What are the benefits of ETT?</Heading>
+                <UnorderedList>
+                    <ListItem>
+                        Creating a healthy climate for all - reducing awards and appointments for harassers, while recognizing that a person 
+                        may learn and correct past behaviors, benefiting everyone.  
+                    </ListItem>
+                    <ListItem>
+                        Ethically treating everyone - making it easier for an entity that made a misconduct finding (the most reliable source) 
+                        to share it with an entity that requests it via ETT.  Doing so with care for sensitive information and without shaming or whisper campaigns.
+                    </ListItem>
+                    <ListItem>
+                        Minimizing legal and enterprise risk for all involved: organizations maintain independence in all policy- and decision making; candidates provide 
+                        consent for disclosures; and disclosures are limited to useful but hard to dispute facts—the kind and date of a misconduct finding.
+                    </ListItem>
+                    <ListItem>
+                        Enhancing efficiency in consenting to and requesting disclosures – a consent has a 10-year life and can be used to request and provide disclosures 
+                        throughout (unless rescinded early) and ETT automates requests for disclosures and reminders.
+                    </ListItem>
+                    <ListItem>
+                        ETT never receives disclosures–only the organizations that request them using ETT do - there is no centralized shame list or conduct record. 
+                    </ListItem>
+                </UnorderedList>
+                <Heading my="4" size="md">How does the Ethical Transparency Tool work?</Heading>
+                <Box display="flex" justifyContent="center" mt="4">
+                    <Image src="/ett-diagram.png" alt="ETT Diagram" />
+                </Box>
+                <Heading my="4" size="md">What information is retained in the ETT?</Heading>
+                <Text>
+                    Organizations’ and individuals’ registration to use ETT and individuals’ consent forms are stored in ETT.  Candidate professional affiliations 
+                    (their employers, appointing and honoring organizations, and societies) and organization requests for disclosures are deleted as soon as ETT sends 
+                    them and two reminders. (A limited archival record of the transmission is kept behind a firewall.)  ETT is a conduit, not a records repository. 
+                </Text>
+            </Box>
         </>
     );
 }
