c8d137b6c424c88c3af55319f16bcd9d22c4502e
Jonathan Williams <tamlin@mac.com>
2025-03-11 11:01:35 -0400

    move the fetch data function out of the useEffect hook
    for one because it makes it easier to read, but also because then other components can invoke it independently of the initial page load

--- src/pages/authorized/amendRegistrationPage.jsx
+++ src/pages/authorized/amendRegistrationPage.jsx
@@ -34,75 +34,75 @@ export default function AmendRegistrationPage() {
     // Get the user whose role is 'RE_AUTH_IND', which is the authorized individual.
     const authUser = userData.entity?.users?.find(user => user.role === 'RE_AUTH_IND') || '';
 
+    async function fetchData() {
+        // appConfig is initially loaded through an api call, which won't have been completed on the first render, so return early if it's not loaded yet.
+        // Because appConfig is a dependency of this useEffect, fetchData will be called again when appConfig is loaded.
+        if (!appConfig) {
+            setApiState('loading');
+            return;
+        }
+
+        // De-structure useful values from the appConfig.
+        const { cognitoDomain, apiStage, authorizedIndividual: { cognitoID, apiHost } } = appConfig;
+
+        // Check to see if this is a first time login from the cognito redirect, and if so do a signIn.
+        // This workaround has to do with the state and code_verifier, which aren't part of the sign up flow.
+        if ( searchParams.get('action') === 'post-signup' && searchParams.has('code') ) {
+            // Any existing login cookies will get in the way, so clear them first.
+            Cookies.remove('EttAccessJwt');
+            Cookies.remove('EttIdJwt');
+
+            // Set a flag in localStorage to indicate first login.
+            window.localStorage.setItem('firstLogin', 'true');
+
+            // Sign in does a window.location redirect, so execution will stop here.
+            signIn( cognitoID, 'auth-ind', cognitoDomain );
+        }
+
+        if (searchParams.has('code') && Cookies.get('EttAccessJwt') === undefined) {
+            // If this exists, then there is a sign in request, so use the code to get the tokens and store them as cookies.
+
+            await exchangeAuthorizationCode( cognitoDomain, cognitoID, 'auth-ind');
+
+            // Use setSearchParams to empty the search params once exchangeAuthorizationCode is done with them.
+            setSearchParams({});
+        }
+
+        const accessToken = Cookies.get('EttAccessJwt');
+        const idToken = Cookies.get('EttIdJwt');
+
+        if (!accessToken || !idToken) {
+            // Set the apiState to 'not-logged-in' to provide feedback
+            setApiState('not-logged-in');
+            return;
+        }
+
+        // If there are login tokens, get the user data.
+        const decodedIdToken = JSON.parse(atob(idToken.split('.')[1]));
+        setAuthorizedInfo(decodedIdToken);
+
+        // Get the consenter list and user data.
+        setApiState('loading');
+
+        // Get the user data from the API and store it in local state.
+        const authIndResponse = await lookupAuthIndAPI(apiHost, apiStage, accessToken, decodedIdToken.email);
+        setUserData(authIndResponse.payload.user);
+
+        // Also set the user context for the avatar in the header.
+        setUser(authIndResponse.payload.user);
+
+        // Check if this is the first login by looking for the flag in sessionStorage.
+        if (window.localStorage.getItem('firstLogin')) {
+            setFirstLogin(true);
+            window.localStorage.removeItem('firstLogin');
+        }
+
+        // Need to add error checking, but I'm not yet sure all these components will stay on the same page.
+        setApiState('success');
+    };
+
 
     useEffect(() => {
-        const fetchData = async () => {
-            // appConfig is initially loaded through an api call, which won't have been completed on the first render, so return early if it's not loaded yet.
-            // Because appConfig is a dependency of this useEffect, fetchData will be called again when appConfig is loaded.
-            if (!appConfig) {
-                setApiState('loading');
-                return;
-            }
-
-            // De-structure useful values from the appConfig.
-            const { cognitoDomain, apiStage, authorizedIndividual: { cognitoID, apiHost } } = appConfig;
-
-            // Check to see if this is a first time login from the cognito redirect, and if so do a signIn.
-            // This workaround has to do with the state and code_verifier, which aren't part of the sign up flow.
-            if ( searchParams.get('action') === 'post-signup' && searchParams.has('code') ) {
-                // Any existing login cookies will get in the way, so clear them first.
-                Cookies.remove('EttAccessJwt');
-                Cookies.remove('EttIdJwt');
-
-                // Set a flag in localStorage to indicate first login.
-                window.localStorage.setItem('firstLogin', 'true');
-
-                // Sign in does a window.location redirect, so execution will stop here.
-                signIn( cognitoID, 'auth-ind', cognitoDomain );
-            }
-
-            if (searchParams.has('code') && Cookies.get('EttAccessJwt') === undefined) {
-                // If this exists, then there is a sign in request, so use the code to get the tokens and store them as cookies.
-
-                await exchangeAuthorizationCode( cognitoDomain, cognitoID, 'auth-ind');
-
-                // Use setSearchParams to empty the search params once exchangeAuthorizationCode is done with them.
-                setSearchParams({});
-            }
-
-            const accessToken = Cookies.get('EttAccessJwt');
-            const idToken = Cookies.get('EttIdJwt');
-
-            if (!accessToken || !idToken) {
-                // Set the apiState to 'not-logged-in' to provide feedback
-                setApiState('not-logged-in');
-                return;
-            }
-
-            // If there are login tokens, get the user data.
-            const decodedIdToken = JSON.parse(atob(idToken.split('.')[1]));
-            setAuthorizedInfo(decodedIdToken);
-
-            // Get the consenter list and user data.
-            setApiState('loading');
-
-            // Get the user data from the API and store it in local state.
-            const authIndResponse = await lookupAuthIndAPI(apiHost, apiStage, accessToken, decodedIdToken.email);
-            setUserData(authIndResponse.payload.user);
-
-            // Also set the user context for the avatar in the header.
-            setUser(authIndResponse.payload.user);
-
-            // Check if this is the first login by looking for the flag in sessionStorage.
-            if (window.localStorage.getItem('firstLogin')) {
-                setFirstLogin(true);
-                window.localStorage.removeItem('firstLogin');
-            }
-
-            // Need to add error checking, but I'm not yet sure all these components will stay on the same page.
-            setApiState('success');
-        };
-
         fetchData();
     }, [appConfig]);
 
