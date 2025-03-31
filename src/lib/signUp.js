f2995d186714683ecab9b1aa750de79636d0fdf1
Jonathan Williams <tamlin@mac.com>
2025-03-31 11:33:00 -0400

    adjust terminology

--- src/lib/signUp.js
+++ src/lib/signUp.js
@@ -1,13 +1,13 @@
 import queryString from 'query-string';
 
 // Constructs a sign up URL and redirects the user to it.
-function signUp( cognitoDomain, email, clientID, redirectRole ) {
+function signUp( cognitoDomain, email, clientID, redirectUri ) {
     const params = {
         username: email,
         client_id: clientID,
         response_type: 'code',
         scope: 'email openid phone',
-        redirect_uri: window.location.origin + '/' + redirectRole,
+        redirect_uri: window.location.origin + '/' + redirectUri,
     };
 
     const signUpQueryString = queryString.stringify(params);
