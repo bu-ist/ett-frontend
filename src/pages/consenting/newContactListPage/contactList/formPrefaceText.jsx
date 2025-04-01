f6f4300e09536094b23bc2e84d7bb1d55e0ce78c
Jonathan Williams <tamlin@mac.com>
2025-04-01 14:25:56 -0400

    drop references to dashboard for now

--- src/pages/consenting/newContactListPage/contactList/formPrefaceText.jsx
+++ src/pages/consenting/newContactListPage/contactList/formPrefaceText.jsx
@@ -5,7 +5,7 @@ export function OtherFormPrefaceText() {
     return (
         <Text mb="6">
             This Full Exhibit Form is incorporated into my Consent Form, 
-            available from <Link as={ReactRouterLink} to="/consenting" textDecoration="underline">my dashboard.</Link> This Exhibit Form 
+            available from <Link as={ReactRouterLink} to="/consenting" textDecoration="underline">here.</Link> This Exhibit Form 
             provides an up-to-date list of the names and contacts for my known 
             Consent Recipients (also called Affiliates) on the date of this 
             Exhibit Form—but NOT my current employers and appointing organizations.  
@@ -19,7 +19,7 @@ export function CurrentFormPrefaceText() {
     return (
         <Text mb="6">
             This Current Employer(s) Exhibit Form is incorporated into my Consent Form
-            available from <Link as={ReactRouterLink} to="/consenting" textDecoration="underline">my dashboard.</Link> This Exhibit Form
+            available from <Link as={ReactRouterLink} to="/consenting" textDecoration="underline">here.</Link> This Exhibit Form
             provides an up-to-date list of the name(s) and contact(s) for my known 
             Current Employers and other Organizations where I hold appointments on 
             the date of this Exhibit.  They are among my Consent Recipients (also called Affiliates). 
@@ -33,7 +33,7 @@ export function BothFormPrefaceText() {
     return (
         <Text mb="6">
             This Full Exhibit Form is incorporated into my Consent Form
-            available from <Link as={ReactRouterLink} to="/consenting" textDecoration="underline">my dashboard.</Link> This Exhibit Form
+            available from <Link as={ReactRouterLink} to="/consenting" textDecoration="underline">here.</Link> This Exhibit Form
             provides an up-to-date list of the name(s) and contact(s) for my known 
             Consent Recipients (also called Affiliates) on the date of this Exhibit Form.  
             The definitions in the Consent Form also apply to this Exhibit Form. My known Consent Recipient(s) are: 
