f66755cb5d645e1cb06c9a84d58070eeeb5821f3
Jonathan Williams <tamlin@mac.com>
2025-05-13 17:05:30 -0400

    comment out unimplemented terminate button
    and add a note how it could be implemented if it needs to be brought back.

--- src/pages/authorizedPage/entityInfoCard/terminateButtonModal.jsx
+++ src/pages/authorizedPage/entityInfoCard/terminateButtonModal.jsx
@@ -1,3 +1,8 @@
+// This component has been removed, and was never fully implemented.
+// It is left here in case it is needed in the future.
+// The relevate api call can be implemented with a payload of:
+// { task: 'demolish-entity', parameters: { entity_id: <entity_id> }
+
 import { Button, ButtonGroup, Divider, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import { AiOutlineClose } from 'react-icons/ai';
 
