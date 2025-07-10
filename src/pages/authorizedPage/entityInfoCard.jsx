3f1900bf9db5270bb3a948fba9f9a234bfff47be
Jonathan Williams <tamlin@mac.com>
2025-07-10 12:10:23 -0400

    Displays pending invitation status
    Improves the entity information card by displaying the status of pending invitations for administrative support professionals and authorized individuals.
    
    Shows the timestamp when the invitation was sent, providing more clarity to the user.

--- src/pages/authorizedPage/entityInfoCard.jsx
+++ src/pages/authorizedPage/entityInfoCard.jsx
@@ -2,6 +2,7 @@
 /* Ideally this would be a shared component, look to refactor this in the future. */
 
 import { Card, CardHeader, Heading, CardBody, Text, HStack, Box, StackDivider, ButtonGroup } from "@chakra-ui/react";
+import { formatTimestamp } from '../../lib/formatting/formatTimestamp';
 
 import AmendModalButton from './entityInfoCard/amendButtonModal';
 import TerminateButtonModal from './entityInfoCard/terminateButtonModal';
@@ -14,6 +15,13 @@ export default function EntityInfoCard({ entityInfo }) {
     
     // If there is already another Authorized Individual, get the email of the user whose role is 'RE_AUTHORIZED'.
     const authUser = entityInfo?.users?.find(user => user.role === 'RE_AUTH_IND') || '';
+
+    // Get any pending invitation to administative support professionals ( where role is 'RE_ADMIN' )
+    const pendingAdminInvitation = entityInfo?.pendingInvitations?.find(invitation => invitation.role === 'RE_ADMIN') || '';
+
+    // Get any pending invitation to authorized individuals ( where role is 'RE_AUTH_IND' )
+    const pendingAuthInvitation = entityInfo?.pendingInvitations?.find(invitation => invitation.role === 'RE_AUTH_IND') || '';
+
 
     const { entity_name } = entityInfo;
 
@@ -33,19 +41,23 @@ export default function EntityInfoCard({ entityInfo }) {
                                 <Text>{adminUser.email}</Text>
                                 <Text>{adminUser.phone_number}</Text>
                             </>
+                        ) : pendingAdminInvitation !== '' ? (
+                            <Text>Invitation pending (sent {formatTimestamp(pendingAdminInvitation.sent_timestamp)})</Text>
                         ) : (
                             <Text>Currently vacant, click &quot;Amend&quot; to send new invitation.</Text>
                         )}
                     </Box>
                     <Box>
                         <Heading as="h4" mb="2" size="sm">Authorized Individuals</Heading>
-                        {authUser ?  (
+                        {authUser ? (
                             <>
                                 <Text>{authUser.fullname}</Text>
                                 <Text>{authUser.title}</Text>
                                 <Text>{authUser.email}</Text>
                                 <Text>{authUser.phone_number}</Text>
                             </>
+                        ) : pendingAuthInvitation !== '' ? (
+                            <Text>Invitation pending (sent {formatTimestamp(pendingAuthInvitation.sent_timestamp)})</Text>
                         ) : (
                             <Text>Second Authorized Individual has not yet registered.</Text>
                         )}
