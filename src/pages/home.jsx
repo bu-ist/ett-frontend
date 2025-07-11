191e3f42d013b92680a1d26adfe5a8d29788ef38
Jonathan Williams <tamlin@mac.com>
2025-07-11 16:22:10 -0400

    add underline to links

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -74,8 +74,8 @@ export default function Home() {
                 <Text>
                     ETT is an ethical and efficient communication tool for societies, colleges, and universities to lead by helping to create a norm of 
                     transparency about findings (not allegations) of individuals’ misconduct (sexual/gender and race/ethnicity, as well as financial, 
-                    scientific/research, and licensure), wherever it occurs.  ETT is designed to implement <a href="https://www.aau.edu/key-issues/campus-climate-and-safety/aau-principles-preventing-sexual-harassment-academia">AAU’s harassment prevention principles</a> and the 
-                    recommendations of <a href="https://nap.nationalacademies.org/catalog/24994/sexual-harassment-of-women-climate-culture-and-consequences-in-academic">NASEM’s June 2018 report</a> on sexual harassment of women in academia and to support inclusive learning and research for all talent.   
+                    scientific/research, and licensure), wherever it occurs.  ETT is designed to implement <a href="https://www.aau.edu/key-issues/campus-climate-and-safety/aau-principles-preventing-sexual-harassment-academia" style={{ textDecoration: 'underline' }}>AAU’s harassment prevention principles</a> and the 
+                    recommendations of <a style={{ textDecoration: 'underline' }} href="https://nap.nationalacademies.org/catalog/24994/sexual-harassment-of-women-climate-culture-and-consequences-in-academic">NASEM’s June 2018 report</a> on sexual harassment of women in academia and to support inclusive learning and research for all talent.   
                 </Text>
             </Box>
             <Heading mb="4" size="md">Access ETT</Heading>
