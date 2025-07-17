c8937d4ac0c87f21e5db0098e6974311ea724174
Jonathan Williams <tamlin@mac.com>
2025-07-17 17:02:15 -0400

    add form download component to about page

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -1,4 +1,5 @@
 import { Heading, Text } from "@chakra-ui/react";
+import DownloadBlankForms from "../components/sharedTexts/home-about/downloadBlankForms";
 
 export default function AboutPage() {
     return (
@@ -21,6 +22,7 @@ Aliqua deserunt esse ad sunt proident voluptate minim voluptate anim deserunt ei
 
 Aliqua deserunt esse ad sunt proident voluptate minim voluptate anim deserunt eiusmod consectetur pariatur. Amet non non nisi incididunt officia nostrud laboris. Anim ut occaecat esse aliquip dolor ex cupidatat. Amet esse mollit incididunt consectetur do proident sunt sunt voluptate in magna nostrud aute. Consectetur dolore aliqua deserunt laboris eiusmod ut aute eiusmod. Et ullamco nulla consectetur quis.
             </Text>
+            <DownloadBlankForms />
         </>
     );
 }
