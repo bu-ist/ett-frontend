4383358f0453f9e0d0526007a81b09fb1f82fee2
Jonathan Williams <tamlin@mac.com>
2025-07-17 14:30:18 -0400

    text changes to definitions

--- src/components/sharedTexts/definitions/definitions.jsx
+++ src/components/sharedTexts/definitions/definitions.jsx
@@ -6,7 +6,7 @@ export function EttDefinition() {
         <>
             <Heading as="h3" size="sm">Ethical Transparency Tool (or ETT)</Heading> 
             <Text mb="6">
-                means a tool that enables each Consent Recipient to provide a completed Disclosure Form (or its information) about an individual who has 
+                means a tool that enables each Consent Recipient to provide a completed Disclosure Form (or its information) about a person who has 
                 signed and delivered a “Consent Form” (this form) to ETT.   Each ETT-Registered Entity (RE) retains its independence in policymaking and 
                 decision-making (e.g., on when to use the ETT, how to respond to disclosures, who’s qualified or selected).  Before completing a Consent 
                 Form, an individual completes an ETT “Registration Form” at this link to receive their ETT account. Individuals do so proactively—or when 
@@ -32,7 +32,7 @@ export function PrivilegesDefinition() {
         <>
             <Heading as="h3" size="sm">Privilege(s) or Honor(s)</Heading>
             <Text mb="6">
-                Examples include but are not limited to: elected fellow, elected or life membership; recipient of an honor, award, or an emeritus or endowed role; elected or appointed governance, committee, officer, or leadership role. However, Privilege(s) or Honor(s) do not include basic membership in an academic, professional, or honorary society at an individual’s initiative (i.e., when not elected or awarded).  Other Privilege(s) or Honor(s) that an ETT-Registered Entity identifies as affecting climate, culture or enterprise risk may be included (e.g., volunteer roles).  
+                Examples include but are not limited to: elected fellow, elected or life membership; recipient of an honor, award, or an emeritus or endowed role; elected or appointed governance, committee, officer, or leadership role. However, Privilege(s) or Honor(s) <b>do not</b> include basic membership in an academic, professional, or honorary society at an individual’s initiative (i.e., when not elected or awarded).  Other Privilege(s) or Honor(s) that an ETT-Registered Entity identifies as affecting climate, culture or enterprise risk may be included (e.g., volunteer roles).  
             </Text>
         </>
     );
@@ -56,7 +56,7 @@ export function ConsentRecipientDefinition() {
         <>
             <Heading as="h3" size="sm">Consent Recipient(s)</Heading>
             <Text mb="6">
-                (also called Affiliate(s)) mean the entities referenced in Part B. 1, 2, 3, 4 of the Consent Form.  A Consent Recipient (Affiliate) is the “Disclosing Entity” that completes a Disclosure Form when requested.  For up-to-date information, the person who submits a Consent Form also provides a list of the names of their Consent Recipients (Affiliates), with contacts, using “Exhibit Forms” at this link each time any ETT-Registered Entity is considering the person for a Privilege or Honor, Employment or Role and is anticipating using ETT to make a Disclosure Request about a person who has completed a Consent Form. 
+                (also called Affiliate(s)) mean the entities referenced in Part B. 1, 2, 3, 4 of the Consent Form.  A Consent Recipient (Affiliate) is the <b>“Disclosing Entity”</b> that completes a Disclosure Form when requested.  For up-to-date information, the person who submits a Consent Form also provides a list of the names of their Consent Recipients (Affiliates), with contacts, using <b>“Exhibit Forms”</b> at this link each time any ETT-Registered Entity is considering the person for a Privilege or Honor, Employment or Role and is anticipating using ETT to make a Disclosure Request about a person who has completed a Consent Form. 
             </Text>
         </>
     );
@@ -67,12 +67,12 @@ export function DisclosureFormDefinition({ disclosureFormUrl }) {
         <>
             <Heading as="h3" size="sm">The Disclosure Form</Heading>
             <Text mb="6">
-                means the form at <a style={{ textDecoration: 'underline' }} target="_blank" rel="noopener noreferrer" href={disclosureFormUrl}>this link</a>. <b>Finding of Responsibility</b> is a finding of any one or more of the generic types of 
+                The Disclosure Form is the form at <a style={{ textDecoration: 'underline' }} target="_blank" rel="noopener noreferrer" href={disclosureFormUrl}>this link</a>. <b>Finding of Responsibility</b> is a finding of any one or more of the generic types of 
                 misconduct listed or referenced on the Disclosure Form; it is defined by the Consent Recipient (Affiliate) that made 
                 or adopted the finding under its own policy (see the Disclosure Form for details).  A Disclosure Form is completed when 
-                a Consent Recipient checks the Finding(s) of Responsibility it has made or adopted against a person (with the year(s)) 
+                a Consent Recipient checks the Finding(s) of Responsibility that it has made or adopted against a person (with the year(s)) 
                 or checks &ldquo;No Finding of Responsibility&rdquo; or &ldquo;Will Not Be Responding&rdquo;—and gives the completed Disclosure Form or its 
-                information to a Registered Entity.  
+                information to a Registered Entity that requested it.  
             </Text>
         </>
     );
