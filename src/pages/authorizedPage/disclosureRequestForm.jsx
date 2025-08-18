8e845c27828960af7c7c78411e9a4ab05610f0da
Jonathan Williams <tamlin@mac.com>
2025-08-18 16:15:33 -0400

    move form instructions to a new text box
    out of the FormHelperText.  The additional instructions go away during form validation on error, so taking them out of the formHelper texts keeps them on screen

--- src/pages/authorizedPage/disclosureRequestForm.jsx
+++ src/pages/authorizedPage/disclosureRequestForm.jsx
@@ -107,12 +107,15 @@ export default function DisclosureRequestForm({ entityId, role }) {
                     {!errors.consenterEmail ? (
                         <FormHelperText>
                             Enter the email address of the Consenting Individual for whom disclosures are being 
-                            requested. <b>Use the email address in that person’s Exhibit Form (which will be the 
-                            same address as is in the person’s ETT Registration and Consent Forms).</b>
+                            requested.
                         </FormHelperText>
                     ) : (
                         <FormErrorMessage>{errors.consenterEmail.message}</FormErrorMessage>
                     )}
+                    <Text mt="2" fontWeight="semibold">
+                        Use the email address in that person’s Exhibit Form (which will be the 
+                        same address as is in the person’s ETT Registration and Consent Forms).
+                    </Text>
                 </FormControl>
 
                 <Button colorScheme='blue' my="2em" type="submit" isDisabled={apiState !== 'idle'}>
