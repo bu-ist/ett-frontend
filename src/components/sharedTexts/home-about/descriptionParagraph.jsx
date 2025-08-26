a38edefc2fb9625996947d91e7534f65dd4dff61
Jonathan Williams <tamlin@mac.com>
2025-08-26 17:02:18 -0400

    Opens external links in new tabs

--- src/components/sharedTexts/home-about/descriptionParagraph.jsx
+++ src/components/sharedTexts/home-about/descriptionParagraph.jsx
@@ -1,12 +1,13 @@
 import { Text } from "@chakra-ui/react";
+import { HiOutlineExternalLink } from "react-icons/hi";
 
 export default function DescriptionParagraph() {
     return (
         <Text>
             ETT is an ethical and efficient communication tool for societies, colleges, universities, and individuals to lead by helping to create a norm of 
             transparency about findings (not allegations) of individuals’ misconduct (sexual/gender and race/ethnicity, as well as financial, 
-            scientific/research, and licensure), wherever it occurs.  ETT is designed to implement <a href="https://www.aau.edu/key-issues/campus-climate-and-safety/aau-principles-preventing-sexual-harassment-academia" style={{ textDecoration: 'underline' }}>AAU’s harassment prevention principles</a> and the 
-            recommendations of <a style={{ textDecoration: 'underline' }} href="https://nap.nationalacademies.org/catalog/24994/sexual-harassment-of-women-climate-culture-and-consequences-in-academic">NASEM’s June 2018 report</a> on sexual harassment of women in academia and to support inclusive learning and research for all talent.   
+            scientific/research, and licensure), wherever it occurs.  ETT is designed to implement <a href="https://www.aau.edu/key-issues/campus-climate-and-safety/aau-principles-preventing-sexual-harassment-academia" style={{ textDecoration: 'underline' }} target="_blank" rel="noopener noreferrer">AAU’s harassment prevention principles</a> and the 
+            recommendations of <a style={{ textDecoration: 'underline' }} href="https://nap.nationalacademies.org/catalog/24994/sexual-harassment-of-women-climate-culture-and-consequences-in-academic" target="_blank" rel="noopener noreferrer">NASEM’s June 2018 report</a> on sexual harassment of women in academia and to support inclusive learning and research for all talent.   
         </Text>
     );
 }
